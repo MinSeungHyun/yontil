e1ee4d4ab1ab28ff131eb7d31d6872753da66a88
Seunghyun Min <i@seunghyun.in>
2024-11-13 11:28:34 +0900

    Replace `username` -> `id`

--- src/core/save-login-data.ts
+++ src/core/save-login-data.ts
@@ -1,7 +1,4 @@
-export default async function saveLoginData(
-  username?: string,
-  password?: string
-) {
-  const loginData = btoa(JSON.stringify({ username, password }))
+export default async function saveLoginData(id?: string, password?: string) {
+  const loginData = btoa(JSON.stringify({ id, password }))
   await chrome.storage.local.set({ loginData })
 }
--- src/entry/content-scripts/login-page.ts
+++ src/entry/content-scripts/login-page.ts
@@ -4,8 +4,8 @@ const loginForm = document.getElementById('ssoLoginForm')
 
 loginForm?.addEventListener('submit', (event) => {
   const formData = new FormData(event.target as HTMLFormElement)
-  const username = formData.get('username')?.toString()
+  const id = formData.get('username')?.toString()
   const password = formData.get('password')?.toString()
 
-  saveLoginData(username, password)
+  saveLoginData(id, password)
 })
