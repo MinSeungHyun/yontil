ad82549c45a000aa50c843523f1312a99f250f98
Seunghyun Min <i@seunghyun.in>
2025-03-01 17:57:16 +0900

    Configure tailwind prefix (fixes #29)

--- src/components/refreshing-overlay.tsx
+++ src/components/refreshing-overlay.tsx
@@ -8,9 +8,9 @@ export default function RefreshingOverlay({ id }: RefreshingOverlayProps) {
   return (
     <div
       id={id}
-      className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50"
+      className="yt-fixed yt-inset-0 yt-z-[9999] yt-flex yt-items-center yt-justify-center yt-bg-black/50"
     >
-      <div className="text-xl font-bold text-white">
+      <div className="yt-text-xl yt-font-bold yt-text-white">
         로그인하는 중입니다. 잠시만 기다려주세요.
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,5 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
+  prefix: 'yt-',
 }
