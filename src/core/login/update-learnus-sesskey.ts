0ca451cbc20488644ef2789e7fa21a4458e9a404
Seunghyun Min <i@seunghyun.in>
2025-05-11 23:57:52 +0900

    Refactor updateLearnUsSesskey to early return if no tabs are found

--- src/core/login/update-learnus-sesskey.ts
+++ src/core/login/update-learnus-sesskey.ts
@@ -2,6 +2,11 @@ import { sendMessageToTabs } from '../../utils/tab-message'
 import { LEARNUS_ORIGIN, LEARNUS_URL_PATTERN } from '../constants'
 
 export default async function updateLearnUsSesskey() {
+  const tabs = await chrome.tabs.query({ url: [LEARNUS_URL_PATTERN] })
+  const tabIds = tabs.map((tab) => tab.id).filter((id) => id !== undefined)
+
+  if (tabIds.length === 0) return
+
   const response = await fetch(LEARNUS_ORIGIN, {
     signal: AbortSignal.timeout(5000),
   })
@@ -10,8 +15,6 @@ export default async function updateLearnUsSesskey() {
   const sesskey = text.match(/sesskey":"([^"]+)/)?.[1]
   if (!sesskey) return
 
-  const tabs = await chrome.tabs.query({ url: [LEARNUS_URL_PATTERN] })
-  const tabIds = tabs.map((tab) => tab.id).filter((id) => id !== undefined)
   await sendMessageToTabs(tabIds, {
     type: 'update-learnus-sesskey',
     sesskey,
