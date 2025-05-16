73685a2a476d1f7f00bcfc2bcdc4b12048b556ce
Seunghyun Min <i@seunghyun.in>
2025-05-16 22:37:23 +0900

    Refactor seek bar preview implementation

--- src/core/video/seek-bar-preview.ts
+++ src/core/video/seek-bar-preview.ts
@@ -1,38 +1,22 @@
 import Hls from 'hls.js'
 
+const seekBarPreviewContainerClassName = 'yontil-seek-bar-preview-container'
+
 export function initializeSeekBarPreview() {
-  const source = document.getElementsByTagName('source')[0]
-  const src = source.src
-
-  const container = document.createElement('div')
-  container.classList.add('yontil-seek-bar-preview-container')
-  document.body.appendChild(container)
-
-  const seekBarPreviewVideo = document.createElement('video')
-  seekBarPreviewVideo.classList.add('yontil-seek-bar-preview')
-  container.appendChild(seekBarPreviewVideo)
-
-  if (Hls.isSupported()) {
-    var hls = new Hls()
-    hls.loadSource(src)
-    hls.attachMedia(seekBarPreviewVideo)
-  } else if (seekBarPreviewVideo.canPlayType('application/vnd.apple.mpegurl')) {
-    seekBarPreviewVideo.src = src
-  }
+  const container = createContainer()
+  const previewVideo = createPreviewVideo(container)
 
   const progressControl = document.querySelector('.vjs-progress-control')
   if (!progressControl) return
 
   progressControl.addEventListener('mousemove', (e) => {
     const event = e as MouseEvent
-
     const progressControlRect = progressControl.getBoundingClientRect()
+
     // The reason why we don't use event.offsetX: https://stackoverflow.com/q/39568498/10202769
     const offsetX = event.x - progressControlRect.left
-
-    const percentage = offsetX / progressControlRect.width
-
-    seekBarPreviewVideo.currentTime = seekBarPreviewVideo.duration * percentage
+    const videoProgress = offsetX / progressControlRect.width
+    previewVideo.currentTime = previewVideo.duration * videoProgress
 
     container.style.top = `${progressControlRect.top - container.offsetHeight - 32}px`
     const leftMin = 16
@@ -50,4 +34,29 @@ export function initializeSeekBarPreview() {
   progressControl.addEventListener('mouseleave', () => {
     container.style.opacity = '0'
   })
+}
+
+function createContainer(): HTMLDivElement {
+  const container = document.createElement('div')
+  container.classList.add(seekBarPreviewContainerClassName)
+  document.body.appendChild(container)
+
+  return container
+}
+
+function createPreviewVideo(container: HTMLElement): HTMLVideoElement {
+  const previewVideo = document.createElement('video')
+  const videoSrc = document.getElementsByTagName('source')[0].src
+
+  if (Hls.isSupported()) {
+    const hls = new Hls()
+    hls.loadSource(videoSrc)
+    hls.attachMedia(previewVideo)
+  } else if (previewVideo.canPlayType('application/vnd.apple.mpegurl')) {
+    previewVideo.src = videoSrc
+  }
+
+  container.appendChild(previewVideo)
+
+  return previewVideo
 }
